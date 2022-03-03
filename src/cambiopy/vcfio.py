cd4d17de2686134fb9f970370d32f3d66cd5942b
Cam Nugent <camnugent@gmail.com>
2022-03-03 15:22:55 -0400

    exploring vcf inputs

--- src/cambiopy/vcfio.py
+++ src/cambiopy/vcfio.py
@@ -17,15 +17,40 @@ def check_vcf(x):
         Raise ValueError("vcf processing fuunction - input must have the extension .vcf or .vcf.gz")
 
 
-def read_genotypes(input_vcf):
+def vcf_to_df(input_vcf):
     """ Read in a vcf file (can be gzipped) using the scikit-allel function. """
 
 
+    vcf=read_vcf(input_vcf)
 
-    vcf=allel.read_vcf(infile,log=sys.stderr)
-    gen=allel.GenotypeArray(vcf['calldata/GT'])
-    samples=vcf['samples']
+
+print("TODO: ")
+print("Figure out what of this information I'd like to store into a dataframe and work to build the data structure")
+print("keep it flexible, but get from this dict of array's to something more tidy")
+
+vcf.keys()
+
+
+vcf['samples'].shape
+vcf['calldata/GT'].shape #these are arrays of [0,0], [1,0],  [1,1] 
+
+vcf['samples']
+vcf['calldata/GT']
+
+
+vcf['variants/ALT'][0]
+vcf['variants/CHROM'][0]
+vcf['variants/FILTER_PASS'][0]
+vcf['variants/ID'][0]
+vcf['variants/POS'][0]
+vcf['variants/QUAL'][0]
+vcf['variants/REF'][0]
 
 
 
 if __name__ == "__main__":
+
+
+    input_vcf = "data/raw/example_file.vcf"
+
+    
