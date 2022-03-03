02259fd0d3c19e8656d371377aaf81651f830ab4
Cam Nugent <nugentc@uoguelph.ca>
2022-03-03 12:07:06 -0400

    generic HMM code

---
+++ src/cambiopy/mltools/hmm/hmm.py
@@ -0,0 +1,76 @@
+import math
+import random
+import numpy as np
+import pandas as pd
+
+from pomegranate import *
+
+
+def hmm(
+		heads_emissions = {"H": 0.7, "T": 0.3},
+		tails_emissions = {"H": 0.334, "T": 0.666}):
+	"""
+
+	Given emission probabilities, initalize a hidden markov model.
+
+	The initial model can be furtuer trained using the viterbi algorithm
+	and simulated label data.
+
+	you'll have to change the transition probabilities to informed priors 
+	"""
+	model = HiddenMarkovModel()
+
+	#these were determined by looking at the real data values
+	dist_heads_emission = DiscreteDistribution(heads_emissions)
+	dist_tails_emission = DiscreteDistribution(tails_emissions)
+
+	heads_state = State(dist_heads_emission, name = "heads")
+	tails_state = State(dist_tails_emission, name = "tails")
+
+
+	model.add_states([heads_state, tails_state])
+
+	model.add_transition(model.start, heads_state, 0.5)
+	model.add_transition(model.start, tails_state, 0.5)
+
+	# note the heads -> and tails -> add up to less than 1
+	# rest of prob goes to exit probability
+	model.add_transition(heads_state, heads_state, 0.75)
+	model.add_transition(heads_state, tails_state, 0.15)
+	model.add_transition(tails_state, heads_state, 0.2) 
+	model.add_transition(tails_state, tails_state, 0.8)
+
+	model.add_transition(tails_state, model.end, 0.1)
+	model.add_transition(heads_state, model.end, 0.1)
+
+	model.bake()
+
+	return model
+
+
+def get_path(sequence, model):
+	""" for a given sequence of observations and HMM model, 
+		generate the predicted series of hidden states using 
+		the viterbi algorithm."""
+	prob, states = model.viterbi(sequence)
+
+	path = [state[1].name for state in states ]
+	return path
+
+if __name__ == '__main__':
+	
+	holstein_hmmr = hmm()
+
+	s = list('HHHHHHHHHTTHHHHHHHH')
+	get_path(s, holstein_hmmr)
+
+	s = list('TTTTTHHHHHHHHTTTTT')
+	get_path(s, holstein_hmmr)
+
+	combos = ['H' * x + 'T' * (91 - x) for x in range(91)]
+
+	combos[55]
+
+	get_path(combos[55], holstein_hmmr)
+
+	get_path(combos[77], holstein_hmmr)
---
+++ src/cambiopy/mltools/hmm/simulate.py
@@ -0,0 +1,111 @@
+import random
+import numpy as np
+import pandas as pd
+
+
+def get_truth_sets(max_flip = 100, one_change = True, 
+						transition = None, samples = None,
+						binary_states = ["H", "T"]):
+	""" generate example strings with either:
+	 		- all of the combinations single transitions
+	 		- random changes
+	 				in this case a transition probability and a number of samples to
+	 				generate must be specified """
+	s1 = binary_states[0]
+	s2 = binary_states[1]
+
+	if one_change == True:
+		combos = [s1 * x + s2 * (max_flip - x) for x in range(max_flip+1)]
+
+	elif one_change == False:
+
+		combos = []
+
+		for i in range(samples):
+
+			current = random.sample([0,1], 1)[0]
+			s = binary_states[current]
+
+			for i in range(1, max_flip):
+				val = random.uniform(0, 1)
+				if val < transition:
+					if current == 0:
+						current = 1
+					elif current== 1:
+						current = 0
+				s+=binary_states[current]
+
+			combos.append(s)
+	return combos
+
+
+def gen_obv(true_val, h_see_t = 0.3820, t_see_h = 0.4625):
+	"""
+	given a true value (hidden state), generate an observed state
+	using the false prediction probabilities."""
+
+	assert true_val == "H" or true_val == "T"
+
+	val = np.random.random_sample()
+
+	if true_val == "H":
+		if val < h_see_t:
+			return "T"
+		else:
+			return "H"
+
+	elif true_val == "T":
+ 		if val < t_see_h:
+ 			return "H"
+ 		else:
+ 			return "T"
+
+
+def get_truth_obvs_pair(true_str):
+	"""Take in a true string, generate a true str, obvs str pair in dict format."""
+
+	new_str = ''
+
+	for c in true_str:
+		obvs_c = gen_obv(c)
+
+		new_str += obvs_c
+
+	out_dict = {'true': true_str, 
+				'obvs' : new_str}
+
+	return out_dict
+
+
+if __name__ == '__main__':
+	
+	simulation_pairs = []
+
+	truth_sets = get_truth_sets()
+	#or
+	rand_truth_sets = get_truth_sets(one_change = False, 
+						transition = 0.1, samples = 100)
+
+
+	for x in range(1000):
+
+		for t in truth_sets:
+			hmm_dict = get_truth_obvs_pair(t)
+
+			simulation_pairs.append(hmm_dict)
+
+
+	out_df = pd.DataFrame(simulation_pairs)
+	out_df.head()
+
+	out_df.to_csv('simulated_data.tsv', sep = '\t', index = False)
+
+	# these will be the ground truth labels.
+	# for each ground truth, create a set simulated observations
+	# insert errors into the observed states based on the probabilities learned
+	# in get_error_data.r
+
+	# will end up with tuples like this
+	# actual: PPPPPPOOOOOOO     obvs: POPPPPOOOPOOO
+
+	#
